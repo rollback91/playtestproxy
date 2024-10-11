511f9e4ed5b5e167e7f3d487a6739b29a13eb75c
Rollback <moccimarco@gmail.com>
2024-10-11 20:52:43 +0200

    ruleparser now converts text symbols to icons

--- app/components/rulesParser.tsx
+++ app/components/rulesParser.tsx
@@ -1,7 +1,23 @@
 export default function RulesParser(text: string) {
     console.log("rules", text);
-    const splittedText = text.split('\n');
+    text = text.toString();
+    const symbols = text.match(/(?<=\{)(.*?)(?=\})/g);
+
+    let ms:string;
+    symbols?.map((symbol) =>{
+        if(symbol === "T") ms = "tap"
+        else ms = symbol.split('/').join('');
+        
+        text = text.replace(
+            `\{${symbol}\}`, 
+            `<i class="ms ms-cost ms-${ms.toLowerCase()}" style="font-size: 0.8em;"></i>`
+        );
+    });
+
+    text = text.replace('\n',`<br\>`);
+
+    const obj = {__html:text};
     return (
-        splittedText.map(text => <>{text} <br /></>)
+        <div dangerouslySetInnerHTML={obj} />
     )
 }
